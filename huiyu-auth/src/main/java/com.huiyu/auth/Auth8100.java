e8097224ef4dfe63c7d0d47dbdfbb93a213437fe
wang0077 <228913299@qq.com>
2023-06-10 04:20:19 +0800

    fix

--- huiyu-auth/src/main/java/com.huiyu/auth/Auth8100.java
+++ huiyu-auth/src/main/java/com.huiyu/auth/Auth8100.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"com.huiyu.service.api.feign"})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Auth8100 {

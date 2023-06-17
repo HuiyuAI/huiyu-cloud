a3cd38480d0fab9eb335364b919b60268c8fc9da
wAnG <228913299@qq.com>
2023-06-17 19:06:22 +0800

    修改JsonTypeHandler

--- huiyu-service/service-core/src/main/java/com/huiyu/service/core/sd/dto/Dto.java
+++ huiyu-service/service-core/src/main/java/com/huiyu/service/core/sd/dto/Dto.java
@@ -3,12 +3,14 @@ package com.huiyu.service.core.sd.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author wAnG
  * @Date 2023-06-12  23:48
  */
 @Data
-public class Dto {
+public class Dto implements Serializable {
     /**
      * 结果图uuid
      */

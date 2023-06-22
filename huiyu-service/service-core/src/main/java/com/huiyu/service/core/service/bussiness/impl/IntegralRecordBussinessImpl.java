6c53d60ae40b9ac7eb91556e7ea950efba9b6a08
lujingeagain <1044005675@qq.com>
2023-06-22 10:56:25 +0800

    并发处理

--- huiyu-service/service-core/src/main/java/com/huiyu/service/core/service/bussiness/impl/IntegralRecordBussinessImpl.java
+++ huiyu-service/service-core/src/main/java/com/huiyu/service/core/service/bussiness/impl/IntegralRecordBussinessImpl.java
@@ -1,12 +1,14 @@
 package com.huiyu.service.core.service.bussiness.impl;
 
 import com.huiyu.service.api.entity.User;
+import com.huiyu.service.core.config.executor.ThreadTransactionManager;
 import com.huiyu.service.core.constant.IntegralOperationRecordEnum;
 import com.huiyu.service.core.constant.IntegralSourceRecordEnum;
 import com.huiyu.service.core.entity.IntegralRecord;
 import com.huiyu.service.core.service.IntegralRecordService;
 import com.huiyu.service.core.service.auth.UserService;
 import com.huiyu.service.core.service.bussiness.IntegralRecordBussiness;
+import com.huiyu.service.core.utils.IdUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -20,6 +22,7 @@ public class IntegralRecordBussinessImpl implements IntegralRecordBussiness {
 
     @Resource
     private IntegralRecordService integralRecordService;
+
 
     /**
      * 更新积分
@@ -43,28 +46,41 @@ public class IntegralRecordBussinessImpl implements IntegralRecordBussiness {
         } else {
             userIntegral -= integral;
         }
+        // todo 并发会导致其他线程操作失败，重试机制
+        boolean startResult = ThreadTransactionManager.startTransaction();
+        if (startResult) {
+            try {
+                // 修改用户积分
+                User user = User.builder()
+                        .id(userId)
+                        .integral(userIntegral)
+                        .build();
+                boolean update = userService.update(user);
 
-        // 修改用户积分 TODO 并发修改
-        User user = User.builder()
-                .id(userId)
-                .integral(userIntegral)
-                .build();
-        boolean update = userService.update(user);
-
-        if (!update) {
-            return false;
+                if (!update) {
+                    return false;
+                }
+                // 记录积分表
+                IntegralRecord integralRecord = IntegralRecord.builder()
+                        .userId(String.valueOf(userId))
+                        .recordNo(IdUtils.getUuId())
+                        .fraction(integral)
+                        .operationType(operation)
+                        .operationSource(source)
+                        .createTime(LocalDateTime.now())
+                        .updateTime(LocalDateTime.now())
+                        .isDelete(0)
+                        .build();
+                boolean isInsert = integralRecordService.insertRecord(integralRecord);
+                if (!isInsert) {
+                    throw new RuntimeException("流水表插入失败");
+                }
+                ThreadTransactionManager.transactionCommit.apply(null, null);
+                return true;
+            } catch (Exception e) {
+                ThreadTransactionManager.transactionRollback.apply(e);
+            }
         }
-        // 记录积分表
-        IntegralRecord integralRecord = IntegralRecord.builder()
-                .userId(String.valueOf(userId))
-                .recordNo("1")
-                .fraction(integral)
-                .operationType(operation)
-                .operationSource(source)
-                .createTime(LocalDateTime.now())
-                .updateTime(LocalDateTime.now())
-                .isDelete(0)
-                .build();
-        return integralRecordService.insertRecord(integralRecord);
+        return false;
     }
 }

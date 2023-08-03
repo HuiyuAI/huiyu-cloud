e3b8bccca287841489996d9ea9d5b0b30f261c5b
Naccl <i@naccl.top>
2023-08-03 18:17:40 +0800

    自定义翻译

---
+++ huiyu-service/service-core/src/main/java/com/huiyu/service/core/utils/translate/CustomTranslateUtils.java
@@ -0,0 +1,18 @@
+package com.huiyu.service.core.utils.translate;
+
+import com.huiyu.service.core.service.SpellbookService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Naccl
+ * @date 2023-08-03
+ */
+@Component
+public class CustomTranslateUtils {
+    @Resource
+    private SpellbookService spellbookService;
+
+
+}
